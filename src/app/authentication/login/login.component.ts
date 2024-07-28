0b69f6fbf161efbabc23aca7a42a7e6a065506e3
Jonas Morales <jonasjosuemo2021@outlook.com>
2024-07-27 18:45:11 -0600

    authentication

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonInputPasswordToggle,
+  IonItem,
+  IonLabel,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    ReactiveFormsModule,
+    IonCard,
+    IonCardTitle,
+    IonCardContent,
+    IonCardHeader,
+    IonInputPasswordToggle,
+  ],
 })
-export default class LoginComponent  implements OnInit {
-
-  constructor() { }
+export default class LoginComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
+  public hide = true;
+  // private authenticationService = inject(AuthenticationService);
+  private formBuider = inject(FormBuilder);
+  public loginForm: FormGroup = this.formBuider.group({
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.minLength(8)]],
+  });
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  loginWithEmailAndPassword() {
+    console.log(this.loginForm.value)
+    
+  }
 }
--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton, IonCard, IonCardTitle, IonCardContent, IonCardHeader, IonInputPasswordToggle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
   standalone: true,
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    ReactiveFormsModule,
+    IonCard,
+    IonCardTitle,
+    IonCardContent,
+    IonCardHeader,
+    IonInputPasswordToggle,
+  ],
 })
-export default class RegisterComponent  implements OnInit {
-
-  constructor() { }
+export default class RegisterComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
+  // private authenticationService = inject(AuthenticationService);
+  private formBuider = inject(FormBuilder);
+  public registerForm: FormGroup = this.formBuider.group({
+    name: [null, Validators.required],
+    lastName: [null, Validators.required],
+    email: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, Validators.minLength(8)]],
+    confirmPassword: [null, [Validators.required, Validators.minLength(8)]],
+  });
 
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  createNewAccount() {
+    console.log(this.registerForm.value);
+  }
 }
